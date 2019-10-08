f75ec96f70e6a65603a28f7cc9a20718afb89ef4
Jason Dobrowner <jason@dev-local.gem.farm>
2019-10-08 16:34:06 -0700

    bordered button

--- ui/src/components/basic/button/Button.js
+++ ui/src/components/basic/button/Button.js
@@ -11,4 +11,17 @@ const Button = ({
   <button type="submit" disabled={disabled} onSubmit={onClick} style={bkgColorWithShadow(backgroundColor)}>{children}</button>
 );
 
+export const BorderedButton = ({
+  disabled,
+  onClick,
+  color,
+  children
+}) => (
+  <button type="submit" disabled={disabled} onSubmit={onClick} style={{ 
+    color,
+    border: `2px solid ${color}`,
+    height: '40px'
+  }}>{children}</button>
+);
+
 export default Button;
--- ui/src/stories/1-Basic.stories.js
+++ ui/src/stories/1-Basic.stories.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { action } from '@storybook/addon-actions';
-import Button from '../components/basic/button/Button';
+import Button, { BorderedButton } from '../components/basic/button/Button';
 import Input from '../components/basic/input/Input';
 import DropdownSelector from '../components/basic/dropdownSelector/DropdownSelector';
 import UIStore from '../stores/UIStore';
@@ -17,6 +17,8 @@ export const buttonShort = () => <Button onClick={action('clicked')} backgroundC
 export const buttonLong = () => <Button onClick={action('clicked')} backgroundColor={Flows.Onramp.primaryColor}>Create Transaction</Button>;
 
 export const buttonDisabled = () => <Button onClick={action('clicked')} backgroundColor={Flows.Onramp.primaryColor} disabled>Create</Button>;
+
+export const borderedButton = () => <BorderedButton onClick={action('clicked')} color={Flows.Onramp.primaryColor}>+ Add new user</BorderedButton>
 
 export const inputWithoutValue = () => <Input value="" placeholder="User Email" />;
 
