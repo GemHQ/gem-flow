76578923add4e282932e1d38545c54a571b0afc7
Jason Dobrowner <jason@dev-local.gem.farm>
2019-10-23 17:33:39 -0700

    upload image, upload modal ui

--- ui/src/components/composite/documentUpload/DocumentUpload.js
+++ ui/src/components/composite/documentUpload/DocumentUpload.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './documentUpload.css';
-import Button from '../../basic/button/Button';
+import { ButtonWithCancel } from '../../basic/button/Button';
 import Dropzone from './Dropzone';
 import DropdownSelector from '../../basic/dropdownSelector/DropdownSelector';
 
@@ -19,32 +19,51 @@ import DropdownSelector from '../../basic/dropdownSelector/DropdownSelector';
 //   ],
 // };
 
+
+const photoIdLabels = {
+  passport: 'Passport',
+  driversLicense: `Driver's License`,
+  governmentId: 'Government Issued ID'
+}
+const photoIdOptions = [
+  { value: 'passport', label: photoIdLabels.passport, className: 'OnrampColor MediumTextSize' },
+  { value: 'driversLicense', label: photoIdLabels.driversLicense, className: 'OnrampColor MediumTextSize' },
+  { value: 'governmentId', label: photoIdLabels.governmentId, className: 'OnrampColor MediumTextSize' },
+];
+const dropdownPlaceholder = 'Choose ID Type';
+
+
 const DocumentUpload = ({ isUploaded, onUpload, onClear }) => {
   const [isUploading, setIsUploading] = useState(false);
-
-
-  const onDrop = useCallback(acceptedFiles => {
-    acceptedFiles.forEach(file => {
-      const reader = new FileReader();
-      reader.onload = e => {
-        onUpload({ id: cuid(), src: e.target.result });
-      };
-      reader.readAsDataURL(file);
-      return file;
-    });
+  const [documentType, setDocumentType] = useState(dropdownPlaceholder);
+  
+  const uploadToProfileForm = files => onUpload({
+    files,
+    type: documentType,
+    description: photoIdLabels[documentType],
   });
   
   return (
-    <div className="MaxButtonWidth">
+    <div>
       {
         isUploaded ?
-        <p></p>
+        <p><span className="OnrampColor BoldText Pointer" onClick={onClear}>clear</span></p>
         :
         <>
-          <Button onClick={() => setIsUploading(true)} type="button">Upload</Button>
+          <DropdownSelector
+            selectedOption={documentType}
+            selectOption={option => {
+              setDocumentType(option);
+              setIsUploading(true);
+            }}
+            options={photoIdOptions}
+            selectedClassName={documentType === dropdownPlaceholder ? 'LightGreyText ThinText MediumTextSize' : 'BlackText ThinText MediumTextSize'}
+          />
           {
             isUploading &&
             <UploadModal
+              documentType={documentType}
+              onUpload={uploadToProfileForm}
               closeModal={() => setIsUploading(false)}
             />
           }
@@ -54,39 +73,52 @@ const DocumentUpload = ({ isUploaded, onUpload, onClear }) => {
   )
 }
 
-const dropdownPlaceholder = 'Select document type';
-const dropdownOptions = [
-  { value: 'passport', label: 'Passport', className: 'OnrampColor MediumTextSize' },
-  { value: 'governmentId', label: 'Government Issued ID', className: 'OnrampColor MediumTextSize' },
-];
 const documentTypeDescriptions = {
-  passport: 'A passport',
-  governmentId: `A government issued id such as a driver's license`
+  passport: 'Please provide the first page of your passport with your photo on it.',
+  driversLicense: `Please provide the front and back of your driver's license.`,
+  governmentId: `Please provide the front and back of your government issued ID.`,
+  shared: 'Make sure the information on the image is clear and legible. Accepted file formats are: png, jpg, pdf, doc, docx.'
 }
 
-const UploadModal = ({ closeModal }) => {
-  const [documentType, setDocumentType] = useState(dropdownPlaceholder);
+const UploadModal = ({ closeModal, onUpload, documentType }) => {
+  const [documents, loadDocuments] = useState([]);
+
+  const onDrop = acceptedFiles => {
+    acceptedFiles.forEach((file, i) => {
+      if (i > 0) return;
+      const reader = new FileReader();
+      reader.onload = e => {
+        const newDocument = {
+          data: e.target.result,
+          media_type: file.type,
+          description: file.name,
+          orientation: documents.length ? 'front' : 'back',
+        };
+        loadDocuments([...documents, newDocument]);
+      };
+      reader.readAsDataURL(file);
+    });
+  };
 
   return (
     <div className="UploadModal">
       <div className="UploadModalBackground" onClick={closeModal} />
       <div className="ModalBox">
-        <h2 className="ModalTitle">Upload Photo ID</h2>
-        <DropdownSelector
-          selectedOption={documentType}
-          selectOption={option => setDocumentType(option)}
-          options={dropdownOptions}
-          selectedClassName={documentType === dropdownPlaceholder ? 'LightGreyText ThinText MediumTextSize' : 'BlackText ThinText MediumTextSize'}
-        />
-        <p className="DocumentTypeDescription">{documentTypeDescriptions[documentType]}</p>
-        <Dropzone />
+        <div className="ModalBoxHeader">
+          <h2 className="ModalTitle">{`Upload ${photoIdLabels[documentType]}`}</h2>
+        </div>
+        <p className="DocumentTypeDescription">{`${documentTypeDescriptions[documentType]} ${documentTypeDescriptions.shared}`}</p>
+        <Dropzone onDrop={onDrop} documents={documents} />
         <div className="ModalButtonContainer">
-          <Button
-            disabled={documentType === dropdownPlaceholder}
+          <ButtonWithCancel
+            primaryColor="#9C27B0"
+            disabled={!documents.length}
             onClick={() => {
+              onUpload(documents);
               closeModal();
             }}
-          >Save</Button>
+            onCancel={closeModal}
+          >Save</ButtonWithCancel>
         </div>
       </div>
     </div>
--- ui/src/components/composite/documentUpload/Dropzone.js
+++ ui/src/components/composite/documentUpload/Dropzone.js
@@ -1,24 +1,31 @@
 import React from "react";
 import { useDropzone } from "react-dropzone";
 
-const Dropzone = ({ onDrop, accept }) => {
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({
-    onDrop,
-    accept
-  });
+const Dropzone = ({ onDrop, documents }) => {
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop, accept: 'image/*' });
 
   return (
     <div className={getClassName("Dropzone", isDragActive)} {...getRootProps()}>
-      <input className="DropzoneInput" {...getInputProps()} />
-      <div className="text-center">
-        {isDragActive ? (
-          <p className="DropzoneContent">Release to drop the files here</p>
-        ) : (
-          <p className="DropzoneContent">
-            Drag 'n' drop some files here, or click to select files
-          </p>
-        )}
-      </div>
+      {
+        documents.length ?
+        <>
+          <img src={documents[0]} className="DocumentImage" />
+          <p>{documents[0].name}</p>
+        </>
+        :
+        <>
+          <input className="DropzoneInput" {...getInputProps()} />
+          <div className="text-center">
+            {isDragActive ? (
+              <p className="OnrampColor ExtraBold">Release to drop the files here</p>
+            ) : (
+              <p className="OnrampColor ExtraBold">
+                Drag 'n' drop some files here, or <span className="Underline">click</span> to select files
+              </p>
+            )}
+          </div>
+        </>
+      }
     </div>
   );
 };
