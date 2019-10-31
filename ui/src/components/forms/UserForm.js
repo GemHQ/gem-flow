2ac818ec8738b1982a3750dfa07f76ae97e6a5ac
Jason Dobrowner <jason@dev-local.gem.farm>
2019-10-31 11:04:48 -0700

    optional email

--- ui/src/components/forms/UserForm.js
+++ ui/src/components/forms/UserForm.js
@@ -3,7 +3,7 @@ import TitleAndValue from '../basic/titleAndValue/TitleAndValue';
 import Input from '../basic/input/Input';
 import { ButtonWithCancel } from '../basic/button/Button';
 import { withPrimaryColor } from '../../stores/StoresUtil';
-const description = `Enter an email address for your new user`;
+const description = `Enter an email address for your new user (optional)`;
 
 export const UserForm = ({ onCancel, onSubmit, primaryColor }) => {
   const [email, setEmail] = useState('');
@@ -11,8 +11,8 @@ export const UserForm = ({ onCancel, onSubmit, primaryColor }) => {
   return (
     <form onSubmit={e => e.preventDefault()}>
       <TitleAndValue title="Enter User's Email" value={description} />
-      <Input placeholder="User Email" value={email} onChange={({ target }) => setEmail(target.value)} autoFocus />
-      <ButtonWithCancel onCancel={onCancel} onClick={() => onSubmit({ email })} disabled={!email.length} primaryColor={primaryColor}>Save User</ButtonWithCancel>
+      <Input placeholder="User Email (Optional)" value={email} onChange={({ target }) => setEmail(target.value)} autoFocus />
+      <ButtonWithCancel onCancel={onCancel} onClick={() => onSubmit({ email })} primaryColor={primaryColor}>Save User</ButtonWithCancel>
     </form>
   )
 }
