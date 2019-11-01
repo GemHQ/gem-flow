2f80de6d8fcf82e2f8b203f6965270b1bf4a48cb
Chris Almeida <chris@gem.co>
2019-11-01 14:16:49 -0700

    connection routes -> use next

--- server/routes/connectionRoutes.js
+++ server/routes/connectionRoutes.js
@@ -5,13 +5,12 @@ const gemClient = require('../gemClient');
 /**
  * Get an account by ID
  */
-router.get('/:user_id', async (req, res) => {
+router.get('/:user_id', async (req, res, next) => {
   try {
     const result = await gemClient.get(`/connections`, req.params);
-    res.json(result);
-  } catch(e) {
-    console.log(e)
-    res.status(500).json({ error: e });
+    return res.json(result);
+  } catch (e) {
+    next(e);
   }
 });
 
