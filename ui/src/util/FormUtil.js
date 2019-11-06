2c45a47fb641dd69525d06ec3e482cd62c89e002
Jason Dobrowner <jason@dev-local.gem.farm>
2019-11-06 11:57:55 -0800

    form util functions

--- ui/src/util/FormUtil.js
+++ ui/src/util/FormUtil.js
@@ -1,3 +1,98 @@
 import isEmail from 'isemail';
 
 export const validateEmail = email => isEmail.validate(email);
+
+export const stringHasLength = str => str.length > 0;
+
+export const onlyNumbers = str => str.replace(/\D/g, '');
+
+export const onlyAlpha = str => str.replace(/[^a-zA-Z]+/g, '');
+
+export const withSSNDashes = str => {
+  const firstPart = str.substring(0, 3);
+  if (str.length < 4) {
+    return firstPart;
+  }
+  const secondPart = str.substring(3, 5);
+  if (str.length < 6) {
+    return `${firstPart}-${secondPart}`;
+  }
+  const thirdPart = trimmed(4)(str.substring(5));
+  return `${firstPart}-${secondPart}-${thirdPart}`;
+};
+
+export const toDateSpacedString = str =>
+  withDateSpacing(onlyNumbers(str));
+
+export const toSSNDashedString = str =>
+  withSSNDashes(onlyNumbers(str));
+
+export const twoUpperAlphas = str =>
+  trimmed(2)(onlyAlpha(str)).toUpperCase();
+
+  export const isUsStateCode = str => {
+    return str.length === 2 && usStateCodes.includes(str);
+  };
+  
+  export const usStateCodes = [
+    'AL',
+    'AK',
+    'AS',
+    'AZ',
+    'AR',
+    'CA',
+    'CO',
+    'CT',
+    'DE',
+    'DC',
+    'FM',
+    'FL',
+    'GA',
+    'GU',
+    'HI',
+    'ID',
+    'IL',
+    'IN',
+    'IA',
+    'KS',
+    'KY',
+    'LA',
+    'ME',
+    'MH',
+    'MD',
+    'MA',
+    'MI',
+    'MN',
+    'MS',
+    'MO',
+    'MT',
+    'NE',
+    'NV',
+    'NH',
+    'NJ',
+    'NM',
+    'NY',
+    'NC',
+    'ND',
+    'MP',
+    'OH',
+    'OK',
+    'OR',
+    'PW',
+    'PA',
+    'PR',
+    'RI',
+    'SC',
+    'SD',
+    'TN',
+    'TX',
+    'UT',
+    'VT',
+    'VI',
+    'VA',
+    'WA',
+    'WV',
+    'WI',
+    'WY',
+  ];
+  
