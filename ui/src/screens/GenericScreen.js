1587cc9e4848ba4253fcdcfe059d21326acae11a
Jason Dobrowner <jason@dev-local.gem.farm>
2019-11-12 13:58:25 -0800

    clean-generic-screen-cmp

--- ui/src/screens/GenericScreen.js
+++ ui/src/screens/GenericScreen.js
@@ -13,7 +13,6 @@ const GenericScreen = ({
   withOpenForm,
   isFetching,
   isPosting,
-  autoCloseForm = true
 }) => {
   const [creatingItem, setCreatingItem] = useState(withOpenForm);
   const startCreatingItem = () => setCreatingItem(true);
@@ -21,38 +20,67 @@ const GenericScreen = ({
 
   if (!numberOfItems && !creatingItem) {
     return isFetching
-    ? <p className="Loading">{`Loading ${itemTitle}s...`}</p>
+    ? <LoadingLabel itemTitle={itemTitle} />
     : <>
         <Button onClick={startCreatingItem} disabled={buttonDisabled}>{`Create New ${itemTitle}`}</Button>
-        { isPosting && <p className="Creating">{`Creating ${itemTitle}...`}</p> }
+        <PostingLabel isPosting={isPosting} itemTitle={itemTitle} />
       </>
   }
 
   return (
     <>
-    {
-      creatingItem
-      ?
-      <>
-        <h2 className="ScreenHeading">{`Creating ${itemTitle}`}</h2>
-        <ItemForm
-          onCancel={stopCreatingItem}
-          onSubmit={item => {
-            createItem(item);
-            autoCloseForm && stopCreatingItem();
-          }}
-        />
-      </>
-      :
-      <div className="FlexAlignCenter SpaceBetween">
-        <h2 className="ScreenHeading noPadding">{`${numberOfItems} ${itemTitle}${numberOfItems > 1 ? 's' : ''}`}</h2>
-        <BorderedButton color={primaryColor} onClick={startCreatingItem}>{`+ Add new ${itemTitle.toLowerCase()}`}</BorderedButton>
-      </div>
-    }
-    { isPosting && <p className="Creating">{`Creating ${itemTitle}...`}</p> }
-    {children}
+      <ItemFormControl 
+        ItemForm={ItemForm}
+        itemTitle={itemTitle}
+        creatingItem={creatingItem}
+        createItem={createItem}
+        startCreatingItem={startCreatingItem}
+        stopCreatingItem={stopCreatingItem}
+        numberOfItems={numberOfItems}
+        primaryColor={primaryColor}
+      />
+      <PostingLabel isPosting={isPosting} itemTitle={itemTitle} />
+      {children}
     </>
   )
+}
+
+const ItemFormControl = ({
+  ItemForm,
+  itemTitle,
+  creatingItem,
+  createItem,
+  startCreatingItem,
+  stopCreatingItem,
+  numberOfItems,
+  primaryColor
+}) => {
+  if (creatingItem) return (
+    <>
+      <h2 className="ScreenHeading">{`Creating ${itemTitle}`}</h2>
+      <ItemForm
+        onCancel={stopCreatingItem}
+        onSubmit={item => {
+          createItem(item);
+          stopCreatingItem();
+        }}
+      />
+    </>
+  )
+
+  return (
+    <div className="FlexAlignCenter SpaceBetween">
+      <h2 className="ScreenHeading noPadding">{`${numberOfItems} ${itemTitle}${numberOfItems > 1 ? 's' : ''}`}</h2>
+      <BorderedButton color={primaryColor} onClick={startCreatingItem}>{`+ Add new ${itemTitle.toLowerCase()}`}</BorderedButton>
+    </div>
+  )
+}
+
+const LoadingLabel = ({ itemTitle }) => <p className="Loading">{`Loading ${itemTitle}s...`}</p>;
+
+const PostingLabel = ({ isPosting, itemTitle }) => {
+  if (isPosting) return <p className="Creating">{`Creating ${itemTitle}...`}</p>
+  return null;
 }
 
 const mapStateToProps = ({ flowStore, uiStore }) => ({ 
