38f019e7d1abe6b46bdc1a3318be83c7758ca891
Jason Dobrowner <jason@dev-local.gem.farm>
2019-11-15 11:00:22 -0800

    ui store comments

--- ui/src/stores/UIStore.js
+++ ui/src/stores/UIStore.js
@@ -1,4 +1,4 @@
-import { observable, action, computed, decorate } from 'mobx';
+import { observable, action, computed } from 'mobx';
 import { Flows, ScreenNames } from './Constants';
 import { 
   persistSelectedFlowId,
@@ -8,9 +8,9 @@ import {
 } from '../util/PersistUtil';
 
 class UIStore {
-  flow = Flows.Onramp;
-  currentScreen = ScreenNames.USER;
-  initialScreenStates = new Map();
+  @observable flow = Flows.Onramp;
+  @observable currentScreen = ScreenNames.USER;
+  @observable initialScreenStates = new Map();
 
   constructor() {
     const persistedFlowId = getPersistedFlowId();
@@ -19,45 +19,39 @@ class UIStore {
     if (persistedScreen) this.setCurrentScreen(persistedScreen);
   }
 
-  setFlow = flowId => {
+  @action setFlow = flowId => {
     this.flow = Flows[flowId];
     persistSelectedFlowId(flowId);
   };
 
-  setCurrentScreen = (screen, initialState) => {
+  @action setCurrentScreen = (screen, initialState) => {
     this.currentScreen = screen;
     initialState && this.initialScreenStates.set(screen, initialState);
     persistCurrentScreen(screen);
   }
 
-  get primaryColor() {
+  // each flow has it's own color
+  @computed get primaryColor() {
     return this.flow.primaryColor;
   }
 
-  get dropdownOptions() {
+  // for the flow dropdown selector on first screen
+  @computed get dropdownOptions() {
     return Object.values(Flows)
       .filter(flow => flow.id !== this.flow.id)
       .map(flow => ({ value: flow.id, label: flow.dropdownTitle, className: flow.colorClassname }));
   }
 
-  get showInstructions() {
+  // do the dropdown selector and instruction show
+  @computed get showInstructions() {
     return this.currentScreen === ScreenNames.USER;
   }
 
-  get withOpenForm() {
+  // if a screen should render with an open form
+  @computed get withOpenForm() {
     const initialState = this.initialScreenStates.get(this.currentScreen);
     return initialState && initialState.withOpenForm;
   }
 }
-
-decorate(UIStore, {
-  flow: observable,
-  currentScreen: observable,
-  setFlowName: action,
-  setCurrentScreen: action,
-  primaryColor: computed,
-  dropdownOptions: computed,
-  withOpenForm: computed,
-});
 
 export default UIStore;
