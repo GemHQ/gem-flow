9841913b964373b2fd653f3dc180e612a52df8cc
Chris Almeida <chris@gem.co>
2019-11-22 13:34:03 -0800

    add widget initial iframe

---
+++ client/src/components/WidgetIframe.js
@@ -0,0 +1,50 @@
+import React, { useEffect } from 'react';
+
+/**
+ * Receive an event/message from the child iframe.
+ */
+const receiveChildMessage = event => {
+  try {
+    const [msg, data] = event.data.split(':');
+
+    switch (msg) {
+      case 'LINK_CONNECTION_ID':
+        // TODO: link creds
+        console.log('Link Credentials', data);
+      default:
+        console.warn('Unknown child message received:', msg);
+        return;
+    }
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+export default props => {
+  useEffect(() => {
+    window.addEventListener('message', receiveChildMessage, false);
+
+    return () => {
+      window.removeEventListener('message');
+    };
+  }, [window, receiveMessage]);
+
+  return (
+    <iframe
+      src="http://localhost:3000/connect"
+      allowfullscreen="true"
+      allowtransparency="true"
+      frameBorder="0"
+      style={{
+        backgroundColor: 'transparent',
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        height: '100vh',
+        width: '100%',
+        zIndex: 1,
+      }}
+      {...props}
+    />
+  );
+};
