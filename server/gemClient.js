31044ddecddea6bf5a5fb54aa6046a5b01f0ba68
Jason Dobrowner <jason@dev-local.gem.farm>
2019-12-04 10:14:27 -0800

    point gem node to stage

--- server/gemClient.js
+++ server/gemClient.js
@@ -7,10 +7,6 @@ const { GEM_API_KEY, GEM_API_SECRET } = process.env;
 const gemClient = new GemClient({
   apiKey: GEM_API_KEY,
   secretKey: GEM_API_SECRET,
-  options: {
-    timeout: 60000,
-  },
-  baseUrl: 'https://vgs-dev.gem.farm'
 });
 
 module.exports = gemClient;
