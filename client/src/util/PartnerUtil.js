03aa2d596d2937014bcf527d4906e5b049b48229
Jason Dobrowner <jason@dev-local.gem.farm>
2019-12-12 16:47:08 -0800

    remove coinbase oauth launch helpers

--- client/src/util/PartnerUtil.js
+++ client/src/util/PartnerUtil.js
@@ -1,13 +1,3 @@
-import { REACT_APP_COINBASE_CLIENT_ID } from '../constants/Env';
-
-const COINBASE_OAUTH_REDIRECT = encodeURI(
-  `https://www.coinbase.com/oauth/authorize?client_id=${REACT_APP_COINBASE_CLIENT_ID}&redirect_uri=http://localhost:3000/coinbase/oauth&response_type=code&scope=wallet:user:read,wallet:accounts:read,wallet:deposits:read,wallet:withdrawals:read,wallet:buys:read,wallet:sells:read,wallet:orders:read,wallet:transactions:read,wallet:transactions:send&meta[send_limit_amount]=1&meta[send_limit_period]=day&meta[send_limit_currency]=USD&account=all`
-);
-
-export const startCoinbaseOauthFlow = () => {
-  window.location = COINBASE_OAUTH_REDIRECT;
-};
-
 export const getOauthCode = () => {
   const queryParams = new URLSearchParams(window.location.search);
   const code = queryParams.get('code');
