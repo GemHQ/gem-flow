7edd02191fee9154f57156064405e1548cfadbce
Jason Dobrowner <jason@dev-local.gem.farm>
2019-12-18 13:16:41 -0800

    set onGemReady

--- client/src/components/widgets/WidgetIframe.js
+++ client/src/components/widgets/WidgetIframe.js
@@ -1,11 +1,15 @@
 import { REACT_APP_GEM_API_KEY } from "../../constants/Env";
 
-export const openGemConnect = ({ onSuccess, institution }) => {
-  const GC = new window.Gem.Connect({
-    onSuccess,
+let GC;
+
+window.onGemReady = () => {
+  GC = new window.Gem.Connect({
     apiKey: REACT_APP_GEM_API_KEY,
     partnerName: 'Flow',
     partnerIconUrl: 'https://app-stage.gem.co/images/wallet/icon_demo_wallet@2x.png',
   });
-  GC.open({ institution });
+}
+
+export const openGemConnect = ({ onSuccess, institution }) => {
+  GC.open({ onSuccess, institution });
 }
--- client/src/util/PartnerUtil.js
+++ client/src/util/PartnerUtil.js
@@ -1,2 +1,4 @@
+import { InstitutionIds } from "../stores/Constants";
+
 export const filterPaymentInstitutions = connections =>
-  connections.filter(connection => connection.institution_id !== 'wyre');
+  connections.filter(connection => connection.institution_id !== InstitutionIds.WYRE);
