083c7dfb7f2b5bf49706896284b51d81b0774b3c
Jason Dobrowner <jdobrowner@gmail.com>
2021-06-03 16:53:23 -0700

    clearable input

--- client/src/components/basic/input/Input.js
+++ client/src/components/basic/input/Input.js
@@ -12,11 +12,18 @@ const Input = ({
   readOnly,
   children,
 }) => {
-
   return (
     <div className={`InputContainer ${containerClassName}`}>
       {value.length > 0 && <p className="floatingLabel">{placeholder}</p>}
-      <input className={className} type="text" value={value} placeholder={placeholder} onChange={onChange} autoFocus={autoFocus} readOnly={readOnly} />
+      <input
+        className={className}
+        type="text"
+        value={value}
+        placeholder={placeholder}
+        onChange={onChange}
+        autoFocus={autoFocus}
+        readOnly={readOnly}
+      />
       {children}
     </div>
   );
@@ -30,6 +37,18 @@ export const TipInput = ({ tipText, ...props }) => {
       <div className="InputRightSubcontainer">
         <HelpTip text={tipText} />
       </div>
+    </Input>
+  );
+};
+
+export const ClearableInput = ({ onClear, ...props }) => {
+  return (
+    <Input className="ClearableInput" {...props}>
+      {Boolean(props.value) && (
+        <div className="InputRightSubcontainer">
+          <div onClick={onClear}>X</div>
+        </div>
+      )}
     </Input>
   );
 };
