e72c25cdf51b174e90db7f5e61f127a0cd993174
Jason Dobrowner <jdobrowner@gmail.com>
2021-06-04 14:37:58 -0700

    theming wip

--- client/src/components/basic/button/Button.js
+++ client/src/components/basic/button/Button.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './button.css';
-import { bkgColorWithShadow } from '../../../util/StyleUtil'
+import { bkgColorWithShadow } from '../../../util/StyleUtil';
 import { withPrimaryColor } from '../../../stores/StoresUtil';
 
 const Button = ({
@@ -9,36 +9,42 @@ const Button = ({
   primaryColor,
   marginRight,
   type,
-  children
+  children,
+  className,
 }) => (
   <button
-    className={marginRight ? 'ButtonRightMargin' : ''}
-    type={type || "submit"} 
-    disabled={disabled} 
+    className={`${className} ${marginRight ? 'ButtonRightMargin' : ''}`}
+    type={type || 'submit'}
+    disabled={disabled}
     onClick={onClick}
-    style={bkgColorWithShadow(primaryColor)}>{children}</button>
+    style={bkgColorWithShadow(primaryColor)}
+  >
+    {children}
+  </button>
 );
 
 export default withPrimaryColor(Button);
 
-export const BorderedButton = ({
-  disabled,
-  onClick,
-  color,
-  children
-}) => (
-  <button className="BorderedButton" type="button" disabled={disabled} onClick={onClick} style={{ 
-    color,
-    border: `2px solid ${color}`,
-  }}>{children}</button>
+export const BorderedButton = ({ disabled, onClick, color, children }) => (
+  <button
+    className="BorderedButton"
+    type="button"
+    disabled={disabled}
+    onClick={onClick}
+    style={{
+      color,
+      border: `2px solid ${color}`,
+    }}
+  >
+    {children}
+  </button>
 );
 
-export const ButtonWithCancel = ({
-  onCancel,
-  ...props
-}) => (
+export const ButtonWithCancel = ({ onCancel, ...props }) => (
   <div className="FlexAlignCenter FlexEnd">
-    <p className="Cancel" onClick={onCancel}>Cancel</p>
-    <Button {...props}/>
+    <p className="Cancel" onClick={onCancel}>
+      Cancel
+    </p>
+    <Button {...props} />
   </div>
 );
