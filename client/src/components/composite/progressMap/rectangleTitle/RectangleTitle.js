56c0c517c5319e80ba9f4617137f9878bd368081
Jason Dobrowner <jdobrowner@gmail.com>
2021-06-16 13:28:36 -0700

    progress bar nav blocking

--- client/src/components/composite/progressMap/rectangleTitle/RectangleTitle.js
+++ client/src/components/composite/progressMap/rectangleTitle/RectangleTitle.js
@@ -2,25 +2,46 @@ import React from 'react';
 import './rectangleTitle.css';
 import { observer } from 'mobx-react';
 
-const RectangleTitle = ({ title, subtitle, color, isCurrentScreen, isCompleted, onClick }) => {
-  let boxStyle = {  border: `2px solid #D9D9D9` };
+const RectangleTitle = ({
+  title,
+  subtitle,
+  color,
+  isCurrentScreen,
+  isCompleted,
+  onClick,
+}) => {
+  let boxStyle = {
+    border: `2px solid #D9D9D9`,
+  };
   let titleStyle = { color: '#D9D9D9' };
   if (isCurrentScreen) {
-    boxStyle = {  border: `2px solid ${color}` };
+    boxStyle = { border: `2px solid ${color}` };
     titleStyle = { color };
   } else if (isCompleted) {
-    boxStyle = {  backgroundColor: `#EEF1F4` };
+    boxStyle = { backgroundColor: `#EEF1F4` };
     titleStyle = { color: '#424242' };
   }
 
   return (
-    <div className="RectangleTitleContainer">
-      <div className="TitleBox" style={boxStyle} onClick={onClick}>
+    <div
+      className="RectangleTitleContainer"
+      style={{
+        cursor: isCompleted || isCurrentScreen ? 'pointer' : 'not-allowed',
+      }}
+    >
+      <div
+        className="TitleBox"
+        style={boxStyle}
+        onClick={() => {
+          if (!isCompleted && !isCurrentScreen) return;
+          onClick();
+        }}
+      >
         <p style={titleStyle}>{title}</p>
       </div>
       <p className="RectangleSubtitle">{subtitle}</p>
     </div>
-  )
+  );
 };
 
 export default observer(RectangleTitle);
