a9803b42cef7559736ee3cb4b628ec23f9b66767
Jason Dobrowner <jdobrowner@gmail.com>
2021-06-17 16:03:25 -0700

    credential card

--- client/src/components/cards/ConnectionCard.js
+++ client/src/components/cards/ConnectionCard.js
@@ -3,23 +3,34 @@ import GenericCard from './GenericCard';
 import { withPrimaryColor } from '../../stores/StoresUtil';
 import { formatDate, capitalizeFirstLetter } from '../../util/TextUtil';
 
-const ConnectionCard = ({ connection, onButtonClick, onViewClick, removeConnection, primaryColor }) => (
+const ConnectionCard = ({
+  connection,
+  onButtonClick,
+  onViewClick,
+  removeConnection,
+  primaryColor,
+}) => (
   <GenericCard
     titlesAndValues={[
-      { title: 'CONNECTION_ID', value: connection.id },
-      { title: 'INSTITUTION_NAME', value: capitalizeFirstLetter(connection.institution_id) },
-      { title: 'CREATED_AT', value: formatDate(connection.created_at) },
-      { title: 'STATUS', value: capitalizeFirstLetter(connection.status) },
+      {
+        title: 'INSTITUTION_NAME',
+        value: capitalizeFirstLetter(connection.exchangeId),
+      },
+      { title: 'PROXY_TOKEN', value: connection.proxyToken },
+      { title: 'CREATED_AT', value: formatDate(connection.createdAt) },
     ]}
     buttonText="View Accounts"
     onButtonClick={onButtonClick}
     onViewClick={onViewClick}
     primaryColor={primaryColor}
-    dotsMenuOptions={[{ title: 'Remove connection', onClick: removeConnection }]}
-    iconUrl={buildExchangeIconEndpoint(connection.institution_id)}
+    dotsMenuOptions={[
+      { title: 'Remove connection', onClick: removeConnection },
+    ]}
+    iconUrl={buildExchangeIconEndpoint(connection.exchangeId)}
   />
 );
 
-const buildExchangeIconEndpoint = institutionId => `https://gem-widgets-assets.s3-us-west-2.amazonaws.com/gem-widgets-resources/institutions/icons/square/${institutionId}_square.svg`;
+const buildExchangeIconEndpoint = (institutionId) =>
+  `https://gem-widgets-assets.s3-us-west-2.amazonaws.com/gem-widgets-resources/institutions/icons/square/${institutionId}_square.svg`;
 
 export default withPrimaryColor(ConnectionCard);
