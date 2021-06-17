c7cf2ab9d5a738d08c018c20c8d44cab2faabf86
Jason Dobrowner <jdobrowner@gmail.com>
2021-06-17 16:03:15 -0700

    coinbase oauth config

--- client/src/util/ClientUtil.js
+++ client/src/util/ClientUtil.js
@@ -14,6 +14,7 @@ export const setupClient = async () => {
       },
     },
   });
+  client.spec.servers = [{ url: SERVER_URL, description: 'Local' }];
   console.log(client);
 
   console.log(client.apis);
@@ -53,7 +54,7 @@ export const CoinbaseOAuthParams = {
   provider_name: 'coinbase',
   provider_id:
     '93d7d07b5618cc48c2b2281b500641a716ce206422a5e6ef881465a2d9097ff8',
-  offering_redirect_uri: 'http://localhost:3200/v1/oauth/coinbase/callback',
+  offering_redirect_uri: 'http://localhost:3000/oauth/coinbase/callback',
   oauth2_scopes:
     'wallet:user:read,wallet:accounts:read,wallet:deposits:read,wallet:withdrawals:read,wallet:buys:read,wallet:sells:read,wallet:orders:read,wallet:transactions:read',
 };
