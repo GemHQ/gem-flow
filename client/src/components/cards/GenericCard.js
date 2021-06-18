41a3af707a2084412bfe260d88cb092f377e7699
Jason Dobrowner <jdobrowner@gmail.com>
2021-06-18 15:41:32 -0700

    card with bordered button option

--- client/src/components/cards/GenericCard.js
+++ client/src/components/cards/GenericCard.js
@@ -2,45 +2,79 @@ import React from 'react';
 import './cards.css';
 import TitleAndValue from '../basic/titleAndValue/TitleAndValue';
 import ThreeDots from '../basic/threeDots/ThreeDots';
-import Button from '../basic/button/Button';
+import Button, { BorderedButton } from '../basic/button/Button';
 
-const GenericCard = ({ 
-  titlesAndValues, 
-  onButtonClick, 
-  buttonText, 
-  onViewClick, 
+const GenericCard = ({
+  titlesAndValues,
+  onButtonClick,
+  buttonText,
+  onViewClick,
   viewText,
-  primaryColor, 
-  dotsMenuOptions, 
-  disabled, 
+  primaryColor,
+  dotsMenuOptions,
+  disabled,
   dots = true,
   iconUrl,
-  hideButton
+  hideButton,
+  borderedButton,
 }) => (
   <div className="Card">
     <div className="FlexAlignCenter">
       {iconUrl && <img src={iconUrl} className="AccountIcon" />}
-      {titlesAndValues.map(({ title, value }) => <TitleAndValue key={title} title={title} value={value || '-'} greyTitle smallTitle boldValue rightPadding/>)}
+      {titlesAndValues.map(({ title, value }) => (
+        <TitleAndValue
+          key={title}
+          title={title}
+          value={value || '-'}
+          greyTitle
+          smallTitle
+          boldValue
+          rightPadding
+        />
+      ))}
     </div>
-    {
-      !hideButton &&
+    {!hideButton && (
       <div className="FlexAlignCenter">
         <div className="ColumnCenter">
-          <Button onClick={onButtonClick} primaryColor={primaryColor} disabled={disabled} marginRight>{buttonText}</Button> 
-          {viewText && onViewClick && <UnderButton onClick={onViewClick} primaryColor={primaryColor}>{viewText}</UnderButton>}
+          {borderedButton ? (
+            <BorderedButton
+              onClick={onButtonClick}
+              primaryColor={primaryColor}
+              disabled={disabled}
+              marginRight
+            >
+              {buttonText}
+            </BorderedButton>
+          ) : (
+            <Button
+              onClick={onButtonClick}
+              primaryColor={primaryColor}
+              disabled={disabled}
+              marginRight
+            >
+              {buttonText}
+            </Button>
+          )}
+          {viewText && onViewClick && (
+            <UnderButton onClick={onViewClick} primaryColor={primaryColor}>
+              {viewText}
+            </UnderButton>
+          )}
         </div>
         {dots && <ThreeDots menuOptions={dotsMenuOptions} />}
       </div>
-    }
+    )}
   </div>
 );
 
 const UnderButton = ({ primaryColor, onClick, children }) => (
-  <p 
-    className="SmallText ExtraBold Pointer" 
+  <p
+    className="SmallText ExtraBold Pointer"
     style={{ color: primaryColor, marginTop: '4px' }}
     onClick={onClick}
-  >{children}</p>
-)
+  >
+    {children}
+  </p>
+);
 
 export default GenericCard;
