0def58ac67a6f3202bb7c75e8a805c57742c99af
Jason Dobrowner <jdobrowner@gmail.com>
2021-07-07 10:59:07 -0700

    coinbase redirect uri from env

--- client/src/util/ClientUtil.js
+++ client/src/util/ClientUtil.js
@@ -25,7 +25,7 @@ export const CoinbaseOAuthParams = {
   provider_name: 'coinbase',
   provider_id:
     '93d7d07b5618cc48c2b2281b500641a716ce206422a5e6ef881465a2d9097ff8',
-  offering_redirect_uri: 'http://localhost:3000/oauth/coinbase/callback',
+  offering_redirect_uri: process.env.REACT_APP_COINBASE_REDIRECT_URI,
   oauth2_scopes:
     'wallet:user:read,wallet:accounts:read,wallet:deposits:read,wallet:withdrawals:read,wallet:buys:read,wallet:sells:read,wallet:orders:read,wallet:transactions:read',
 };
